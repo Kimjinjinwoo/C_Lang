61b6a38d75a8024d79f1ae9de4540bbb626c6db1
Kimjinjinwoo <kk234556@gmail.com>
2022-07-11 16:15:02 +0900

    'C_Programming'

--- C_Programming/Practice13/3.c
+++ C_Programming/Practice13/3.c
@@ -9,39 +9,34 @@ int main()
 	scanf("%s", fname);
 
 	FILE* f;
-	if ((fopen_s(&f, fname, "rb")) != 0)
+	if ((fopen_s(&f, fname, "r")) != 0)
 	{
 		puts("¿¡·¯");
 		exit(1);
 	}
-	char str[3];
+	char str[10];
 	int cnt = 0;
 
-	fread(str, sizeof("90") , 1, f);
-	//printf("%d", sizeof("90"));
-	//printf("    %d\n", sizeof("\n"));
-	
+	fgets(str, 10, f);
+
 	while (!feof(f))
 	{
-		str[2] = '\0';
-		puts(str);
-		//printf("%s\n", str);
 		int k = atoi(str);
-		
+		//puts(str);
 		if (k >= 90)
 		{
-			//printf("%d\n", k);
 			cnt += 1;
 			ansList[cnt] = k;
 		}
-		fread(str, 1, 1, f);
-		fread(str, sizeof("90"), 1, f);
+		fgets(str, 10, f);
 	}
 
 	ansList[0] = cnt;
 
 	for (int i = 0; i < cnt + 1; i++)
 		printf("%d\n", ansList[i]);
+
+	fclose(f);
 
 	return 0;
 }
